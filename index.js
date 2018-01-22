9cfd2ec56d3b5a2462953d53d9d34905c8304b4f
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-01-22 07:09:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function theBeatlesPlay (musicians, instruments){
+  var beatlesPlay = []
+  
+  for (var i = 0; i < 4; i++) {
+    beatlesPlay.push(musicians + "plays" + instruments)
+  }
+  return beatlesPlay
+}
+
+function johnLennonFacts(facts) {
+  var jLFacts = []
+  return jLFacts
+}
+
+function iLovetheBeatles (number) {
+  var loveTheBeatles = []
+  do {
+    loveTheBeatles.push("I love the Beatles!")
+    number ++
+    }
+    while (number < 15)
+    return loveTheBeatles
+}
