46614e4ee2e2682af7c5651f0db098a0f7f1f6ae
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-01-22 07:15:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay (musicians, instruments){
   var beatlesPlay = []
   
   for (var i = 0; i < 4; i++) {
-    beatlesPlay.push(musicians + "plays" + instruments)
+    beatlesPlay.push(musicians[i] + "plays" + instruments[i])
   }
   return beatlesPlay
 }
